ec1e801fc54e699785944d60cfbca20c2b9ef3e0
Felipe Gonçalves <felipe@lab-dev.dns.lab.local>
2026-06-30 14:18:32 -0500

    chore: estrutura inicial do bot com docker multi-stage e github actions

---
+++ src/main/java/com/feliperaac/gitcommitmirror/GitCommitMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.feliperaac.gitcommitmirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommitMirrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCommitMirrorApplication.class, args);
+        System.out.println("🚀 Amazon Promo Bot (Git Commit Mirror) iniciado com sucesso!");
+    }
+}
